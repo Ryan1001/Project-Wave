c00f1c5f66f2e54a5246d3e30b31d710ca424dff
Ryan Brill <rbrill@draper.com>
2018-06-04 17:10:19 -0400

    Signal Smoothing and Median filter working
    I used a median filter from that library and set that to process the signal before the running average (smoothing) function. The signal is looking really smooth!

--- SRC/RangeFinderTestCurrent/RangeFinderTestCurrent.ino
+++ SRC/RangeFinderTestCurrent/RangeFinderTestCurrent.ino
@@ -4,7 +4,6 @@
 #include <medianFilter.h>
 #include <newFilter.h>
 
-
 /*
 Project Wave
 by Ryan Brill and Evan Brill
@@ -17,20 +16,24 @@ Last updated June 2, 2018 at home.
         GND: GND
 */
 //---- In this part, I declare all of the variables that I'm going to use in the loop() function----
-  const int numReadings = 10;
+  const int numReadings = 30;
 
   int readings[numReadings];      // the readings from the analog input
   int readIndex = 0;              // the index of the current reading
   int total = 0;                  // the running total
-  int average = 0;                // the average
-  int gatedOutput = 0;                 // output of sensor after gating
 
   int upperLimit = 5000;  //the upper admissible signal reading. Helps prevent wild readings from being passed through.
   int lowerLimit = 0; //you get the idea
 
   int trigPin = 11;    //Trig - green Jumper
   int echoPin = 12;    //Echo - yellow Jumper
-  long duration, cm, inches, thisDuration, prevDuration, output;
+  long duration, output; //duration is the output of the sensor
+  int average = 0;                // the output of the moving average function 
+  int gatedOutput = 0;                 // output of sensor after gating
+  int medianFiltered;     //after we gate the output, we put it through a median Filter
+
+  medianFilter thisMedianFilter;
+
 
   //MIDI variables
   byte noteON = 144;//note on command
@@ -40,6 +43,8 @@ Last updated June 2, 2018 at home.
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
+    thisMedianFilter.begin();
+
   //Define inputs and outputs
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -69,12 +74,14 @@ void loop()
     pinMode(echoPin, INPUT);
     duration = pulseIn(echoPin, HIGH);
 
+    medianFiltered= thisMedianFilter.run(duration);
+
   // -----SMOOTHING ----
     ///// begin smoothing function
     // subtract the last reading:
     total = total - readings[readIndex];
     // read from the sensor:
-    readings[readIndex] = duration;
+    readings[readIndex] = medianFiltered;
     // add the reading to the total:
     total = total + readings[readIndex];
     // advance to the next position in the array:
@@ -99,24 +106,28 @@ void loop()
     if (average < upperLimit && average > lowerLimit) {
       gatedOutput = average;
     };
+
     
-  gatedOutput = map(gatedOutput, 0, 3300, 0, 127);
+//  gatedOutput = map(gatedOutput, 0, 3300, 0, 127);
 
   Serial.println(gatedOutput);
 
   delay(20);
 
-//Send MIDI messages with our sensor data
-MIDImessage(noteON, 60, gatedOutput);//turn note on
+//Send MIDI messages with our sensor data. 
+  // Note that you need to comment out the first line if you want to get Serial Plotter to display correctly.
+
+//MIDImessage(noteON, 60, medianFiltered);//turn note on
 //    delay(300);//hold note for 300ms
 //    MIDImessage(noteOFF, 60, gatedOutput);//turn note off (note on with velocity 0)
 //    delay(200);//wait 200ms until triggering next note
 }
 
-//send MIDI message
+//send MIDI message. This is a helper method that is referenced in loop()
 void MIDImessage(byte command, byte data1, byte data2) {
   Serial.write(command);
   Serial.write(data1);
   Serial.write(data2);
 }
+
 
